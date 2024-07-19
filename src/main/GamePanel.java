2cf5224790692c76db6991cbc8367cb81c687295
LuaComTil <luadefreitasramos@gmail.com>
2024-07-19 10:47:50 -0300

    feat: add fps counter

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -11,6 +11,8 @@ import java.awt.event.KeyListener;
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private int xDelta = 100, yDelta = 100;
+    private int frames = 0;
+    private long lastCheck = 0;
 
     public GamePanel() {
         mouseInputs = new MouseInputs(this);
@@ -21,22 +23,27 @@ public class GamePanel extends JPanel {
 
     public void changeXDelta(int value) {
         this.xDelta +=value;
-        repaint();
     }
 
     public void changeYDelta(int value) {
         this.yDelta +=value;
-        repaint();
     }
 
     public void setRectPos (int x, int y) {
         this.xDelta = x;
         this.yDelta = y;
-        repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawRect(xDelta, yDelta, 50, 50);
+
+        frames++;
+        if (System.currentTimeMillis() - lastCheck >= 1000) {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS:" + frames);
+            frames = 0;
+        }
+        repaint();
     }
 }
